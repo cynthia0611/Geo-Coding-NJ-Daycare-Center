b993ea2442c505d93fff3bc76bf76259f6b4d6eb
Yueming Sun <cynthia0828@gmail.com>
2016-08-02 15:51:13 -0400

    Add files via upload

---
+++ NJ Map.R
@@ -0,0 +1,53 @@
+library(ggmap)
+library(ggrepel)
+
+# library(ggsubplot)
+library(ggplot2)
+library(maps)
+# library(plyr)
+
+# install.packages("ggmap")
+# install.packages("maps")
+# install.packages("ggrepel")
+# install.packages("ggplot2")
+# install.packages("ggsubplot")
+
+# get basic location
+nj <- get_map (location = ("livingston,nj"),
+    maptype="roadmap",
+    zoom=13,
+    color="bw")
+
+nj2 <- get_map (location = ("east hanover,nj"),
+               maptype="roadmap",
+               zoom=12,
+               color="bw")
+
+nj3 <- get_map (location = ("Florham park,nj"),
+               maptype="roadmap",
+               zoom=12,
+               color="bw")
+
+# get basemap
+njBaseMap <- ggmap(nj,extent="device")
+
+# read in geo location data as points
+centers <- read.csv("Z:/Yueming/Licensed_Child_Care_Centers.csv")
+
+# head(data)
+
+# allocate points in the basemap
+map <- njBaseMap +  geom_point(data=centers,
+                               aes(x=lon, y=lat),
+                               size=1.8, 
+                              col="#e6550d",alpha=0.8) + 
+                              scale_size_area(max_size = 9)
+
+# get label besides the points
+map + geom_text_repel(data=centers,
+                              aes(x=lon, y=lat,label = CENTER),
+                              size=3,
+                              color="#081d58",
+                              fontface="bold",
+                              alpha=0.8) 
+
